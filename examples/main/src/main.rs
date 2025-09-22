982fe9504cc68b05997cf96bdcce74de0530db3a
Miodic <60072696+Miodic@users.noreply.github.com>
2025-09-22 12:38:15 +0200

    add basic skeleton

---
+++ examples/main/src/main.rs
@@ -0,0 +1,6 @@
+use lucy::Lucy;
+
+fn main() {
+    let lucy: Lucy = Lucy::new();
+    println!("Hello World Lucy!");
+}
---
+++ lucy/src/lib.rs
@@ -0,0 +1,7 @@
+pub struct Lucy {}
+
+impl Lucy {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
